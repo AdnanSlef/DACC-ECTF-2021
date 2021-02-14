f808f6353810fbf5ad731f034e146d73581b347a
Adrian Self <aself.school@gmail.com>
2021-02-14 22:30:28 +0000

    update helper.py from testing to working

--- sss/helper.py
+++ sss/helper.py
@@ -44,12 +44,10 @@ def create_secrets_before():
 
 # Prepare one SED's secrets file, by deployment ID
 def make_a_secret(depl_id):
-    pubkeys_check = [b'N\xfe+\xe0&\xad\x9dX\x85u\xe9w\x0fV\xa5_s\xc43r\xcdr\x18\xee\x15\xbcS2\x96\xe9q\xa1|\x0b\xcfE\xb8\x931\xef\xc6\xa9;\x92^\x0e5\xce2\x8e\x87\x9a\xfeY1\x08\xd7\x17\x0cn*\x80x}',b'y\x86\x9a\x01\x0ck\xfe\x87[\xee\xc3\xbe\x87S\x89\x08\x13\xf0\xd5\x8b\xc1\xbb\xa5\x10;7\x92\x93\xb5/\xbc\xb32G\xb2\x01E\xde\xb6~\xc7\xebh?)\xa5(\x85X\xb8"\xda q\xa8\xfe\ttX\xe0Z\xc5\xe3\xd0']
     privkeys = [b'\tvs\xa2\xb3\xef\xc4\xd7{\xbe\xb1{\xce\xab\x1b\x8cmS\xfd9\x8b\xb4&\x93%\xfa:s\xa3\x89\xe9\xab',b'[\xb6\x8d\x95\xa9\xe8\xd0\x03\xc2\xfcb\x0c\xad\xc8`\xd9\t4>\x05#\xca\x8bMW\x83\xe4w\xa5\xcd7\x81']
-    pubkeys_computed = [ecc.construct(curve='p256',d=bytes_to_long(privkey)).public_key()._point for privkey in privkeys]
-    pubkeys_computed = [long_to_bytes(p.x) + long_to_bytes(p.y) for p in pubkeys_computed]
-    assert pubkeys_computed == pubkeys_check
-    pubkeys = pubkeys_computed
+    # Calculate public key points
+    pubkeys = [ecc.construct(curve='secp256r1',d=bytes_to_long(privkey)).public_key()._point for privkey in privkeys]
+    pubkeys = [long_to_bytes(point.x) + long_to_bytes(point.y) for point in pubkeys] #format for uECC
 
     secrets = f"""
 #ifndef SECRETS_H
